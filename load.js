dbc3f441d0be70bed69c30ef58880a8238cd08d7
Jonathan Chang <j.a.chang820@gmail.com>
2018-04-04 21:33:47 -0700

    made it mobile-friendly

--- load.js
+++ load.js
@@ -1,4 +1,7 @@
-var title;
+var title = "Machine Learning: R vs Python";
+var menu_timeout;
+var menu_is_open = false;
+var MOBILE_WIDTH = 576;
 
 var build_nav = function() {
 	fetch("menu.xml")
@@ -21,6 +24,10 @@ var build_nav = function() {
 
 var load_topic = function(topic) {
 
+	if (menu_is_open) {
+		expand_menu();
+	}
+
 	$('#main').html("");
 	urls = ["html/" + topic + ".html",
 					"r/" + topic + ".r",
@@ -38,10 +45,9 @@ var load_topic = function(topic) {
 		}).catch(function() {});
 
 	})).then(function(texts) {
-	
-		$('h1').text(title);
+
 		$('#main').html(
-			`<article id="intro">` + texts[0] + `</article>`
+			`<h1>${title}</h1><article id="intro">${texts[0]}</article>`
 		);
 		MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 
@@ -67,14 +73,56 @@ var add_lang = function(content, language, title) {
 }
 
 var expand_menu = function() {
-	$("#nav-arrow").css("transform", "rotate(180deg)");
+	if (!menu_is_open) {
+		$('#nav-expand').attr('id', 'nav-expand-open');
+		if ($(window).width() > MOBILE_WIDTH) {
+			$('#nav-arrow').css("transform", "rotate(180deg)");
+		} else {
+			$('#nav-arrow').css("transform", "rotate(-90deg)");
+		}
+		menu_timeout = setTimeout(function() {
+			$('nav').show();
+			menu_is_open = true;	
+		}, 1750);
+
+	} else {
+		$('#nav-expand-open').attr('id', 'nav-expand');
+		if ($(window).width() > MOBILE_WIDTH) {
+			$('#nav-arrow').css("transform", "rotate(0deg)");
+		} else {
+			$('#nav-arrow').css("transform", "rotate(90deg)");
+		}
+		$('nav').hide();
+		clearTimeout(menu_timeout);
+		menu_is_open = false;
+	}
 }
 
 var close_menu = function() {
-	$("#nav-arrow").css("transform", "rotate(0deg)");
+	$('#nav-arrow').css("transform", "rotate(0deg)");
+	clearTimeout(menu_timeout);
 }
 
 $(document).ready(function() {
 	build_nav();
 	load_topic('home');
+});
+
+$(document).click(function(event) {
+	title = $(event.target).text();
+});
+
+$(window).resize(function() {
+	var deg;
+	if ($(window).width() > MOBILE_WIDTH && menu_is_open) {
+		deg = '180deg';
+	} else if ($(window).width() > MOBILE_WIDTH && !menu_is_open) {
+		deg = '0deg';
+	} else if ($(window).width() <= MOBILE_WIDTH && menu_is_open) {
+		deg = '-90deg';
+	} else {
+		deg = '90deg';
+	}
+
+	$('#nav-arrow').css('transform', `rotate(${deg})`);
 });
